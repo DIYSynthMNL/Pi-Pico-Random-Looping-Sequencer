9c043d179a37466fcc659fb05c0648e3c109114d
Franco Neo Recasata <neorecasata@gmail.com>
2024-03-22 15:01:12 +0800

    updated menu.py

--- lib/menu.py
+++ lib/menu.py
@@ -14,6 +14,10 @@ Menu types:
 - Numerical percent (0-100)
 
 """
+import machine
+from ssd1306 import SSD1306_I2C
+i2c = machine.I2C(0, sda=machine.Pin(16), scl=machine.Pin(17))
+display = SSD1306_I2C(128, 64, i2c)
 
 
 class Menu:
@@ -27,21 +31,59 @@ class SingleSelectVerticalScrollMenu():
     A menu type that lets the user select a single item from a list of strings
 
     """
-    
-    # ! implement oled 
+    global display
 
-    def __init__(self, name: str, selection: str, items=None) -> None:
+    # ! implement oled
+
+    def __init__(self, name: str, selection: str, items=None, total_lines=4) -> None:
         self.name = name
         self.selected = selection
         if items == None:
             items = []
         else:
             self.items = items
+        self.menu_start_index = 0
+        self.total_lines = total_lines
+        self.selected_index = 0
 
     def set_selected(self, selection: str) -> None:
+        # use when button is pressed
         self.selected = selection
 
     def get_selected(self) -> str:
         return self.selected
+    
+    def set_menu_start_index(self, menu_start_index):
+        self.menu_start_index = menu_start_index
+        
+    def set_selected_index(self, selected_index):
+        self.selected_index = selected_index
 
-
+    def display_menu(self) -> None:
+        item_index = 0
+        pixel_y_shift = 20
+        line_height = 10
+        spacer = 2
+        
+        # shift all item positions down to prevent clipping issues
+        display.fill(0)
+        display.text(self.name, 2, 4, 1)
+        display.rect(0, 0, 128, 15, 1)
+        for i in range(min(len(self.items) - self.menu_start_index, self.total_lines)):
+            item_index = self.menu_start_index + i
+            if item_index == self.selected_index:
+                # highlighted item
+                display.fill_rect(0, ((i * (line_height+spacer))-1) +
+                                  pixel_y_shift, 128, line_height, 1)
+                display.text(self.items[item_index], 0,
+                             (i * (line_height+spacer))+pixel_y_shift, 0)
+            else:
+                display.text(self.items[item_index], 0,
+                             (i * (line_height+spacer))+pixel_y_shift, 1)
+        display.show()
+        
+    def scroll(self, index):
+        if index > self.menu_start_index + (self.total_lines-1):
+            self.menu_start_index += 1
+        if index < self.menu_start_index:
+            self.menu_start_index -= 1
--- playground/menu_playground.py
+++ playground/menu_playground.py
@@ -18,21 +18,11 @@ SW Pin 2 to GP20
 
 import machine
 import mcp4725_musical_scales
+import menu as m
 
-from ssd1306 import SSD1306_I2C
 from rotary_irq_rp2 import RotaryIRQ
 from mp_button import Button
 from menu import Menu
-from menu import SingleSelectVerticalScrollMenu
-
-# Hardware I2C using I2C0
-i2c = machine.I2C(0, sda=machine.Pin(16), scl=machine.Pin(17))
-
-# Scan I2C for device(s), prints list of device address(es)
-print(i2c.scan())
-
-# 128x64 OLED Display
-display = SSD1306_I2C(128, 64, i2c)
 
 r = RotaryIRQ(pin_num_clk=18,
               pin_num_dt=19,
@@ -42,43 +32,18 @@ r = RotaryIRQ(pin_num_clk=18,
 
 scale_intervals = mcp4725_musical_scales.get_intervals()
 
-main_menu = Menu("Main menu", ['Single Select', 'Single Select', 'Scale'])
-menu1 = Menu("Single Select", ['M1Item1', 'M1Item2',
-             'M1Item3', 'M1Item4', 'M1Item5', 'M1Item6'])
-menu2 = Menu("Single Select", ['M2Item1', 'M2Item2', 'M2Item3', 'M2Item4', 'M2Item5'])
-menu3 = SingleSelectVerticalScrollMenu(name='Scale', selection='chromatic', items=scale_intervals)
-current_menu = main_menu
-
-selected_index = 0
-menu_start_index = 0
-item_index = 0
-total_lines = 4
-pixel_y_shift = 20
+menu = m.SingleSelectVerticalScrollMenu(
+    name='Scale', selection='chromatic', items=scale_intervals)
+current_menu = menu
 
 
 def button_action(pin, event) -> None:
     global selected_index, menu_start_index
     # set rotary value back to 0 when button is pressed
     if event == Button.PRESSED:
-        change_menu()
-        selected_index = 0
-        menu_start_index = 0
+        current_menu.set_menu_start_index(0)
+        current_menu.set_selected_index(0)
         r.set(value=0)
-
-
-def change_menu() -> None:
-    global current_menu, main_menu, menu1, menu2, menu3
-    if current_menu == main_menu:
-        if selected_index == 0:
-            current_menu = menu1
-        elif selected_index == 1:
-            current_menu = menu2
-        elif selected_index == 2:
-            current_menu = menu3
-    else:
-        current_menu = main_menu
-    update_encoder_range()
-    display_menu(current_menu.items, current_menu.name)
 
 
 b = Button(20, internal_pullup=True, callback=button_action)
@@ -87,31 +52,13 @@ r.set(value=0)
 val_old = -1
 
 
-def display_menu(menu_items, name, line_height=10, spacer=2) -> None:
-    # shift all item positions down to prevent clipping issues
-    display.fill(0)
-    display.text(name, 2, 4, 1)
-    display.rect(0, 0, 128, 15, 1)
-    for i in range(min(len(menu_items) - menu_start_index, total_lines)):
-        item_index = menu_start_index + i
-        if item_index == selected_index:
-            display.fill_rect(0, ((i * (line_height+spacer))-1) +
-                              pixel_y_shift, 128, line_height, 1)
-            display.text(menu_items[item_index], 0,
-                         (i * (line_height+spacer))+pixel_y_shift, 0)
-        else:
-            display.text(menu_items[item_index], 0,
-                         (i * (line_height+spacer))+pixel_y_shift, 1)
-    display.show()
-
-
 def update_encoder_range() -> None:
     # Update min_val and max_val based on the number of menu items
     r.set(min_val=0, max_val=len(current_menu.items) - 1)
 
 
 # Initial display
-display_menu(current_menu.items, current_menu.name)
+menu.display_menu()
 update_encoder_range()
 
 # loop
@@ -124,10 +71,8 @@ while True:
         val_old = val_new
 
         # scroll
-        if val_new > menu_start_index + (total_lines-1):
-            menu_start_index += 1
-        if val_new < menu_start_index:
-            menu_start_index -= 1
-
-        selected_index = val_new
-        display_menu(current_menu.items, current_menu.name)
+        current_menu.scroll(val_new)
+        # set selected index
+        current_menu.set_selected_index(val_new) 
+        # display menu
+        current_menu.display_menu()
