d383b16fcd1acc443d3ec8e278d4ea7cc3624b34
Franco Neo Recasata <neorecasata@gmail.com>
2024-03-25 13:38:33 +0800

    Update menu.py

--- lib/menu.py
+++ lib/menu.py
@@ -14,6 +14,8 @@ Menu types:
 - Numerical percent (0-100)
 
 """
+
+# TODO implement rotary encoder
 import machine
 from ssd1306 import SSD1306_I2C
 
@@ -24,7 +26,7 @@ display = SSD1306_I2C(128, 64, i2c)
 class SingleSelectVerticalScrollMenu():
     """
     A menu type that lets the user select a single item from a list of strings
-    
+
     Note: Selected and highlighted is different from each other
     Highlighted means...
     Selected means...
@@ -33,7 +35,7 @@ class SingleSelectVerticalScrollMenu():
         Initializing:
         menu = m.SingleSelectVerticalScrollMenu(
             name='Scale', selection='chromatic', items=scale_intervals)
-            
+
         On button press:
         if event == Button.PRESSED:
             current_menu.set_menu_start_index(0)
@@ -52,7 +54,7 @@ class SingleSelectVerticalScrollMenu():
 
     global display
 
-    def __init__(self, name: str, selection: str, items=None, total_lines=4) -> None:
+    def __init__(self, name: str, *, selection: str, items=None, total_lines=4) -> None:
         self.name = name
         self.selected = selection
         if items == None:
@@ -70,9 +72,6 @@ class SingleSelectVerticalScrollMenu():
     def set_selected_index(self, selection: int) -> None:
         self.selected = self.items[selection]
 
-    def get_selected(self) -> str:
-        return self.selected
-
     def set_menu_start_index(self, menu_start_index) -> None:
         self.menu_start_index = menu_start_index
 
@@ -87,8 +86,7 @@ class SingleSelectVerticalScrollMenu():
 
         # shift all item positions down to prevent clipping issues
         display.fill(0)
-        # TODO display short version of selected item after name
-        display.text(self.name, 2, 4, 1)
+        display.text(f'{self.name}:{self.selected if len(self.selected) <= 9 else remove_vowels(self.selected)}', 2, 4, 1)
         display.rect(0, 0, 128, 15, 1)
         for i in range(min(len(self.items) - self.menu_start_index, self.total_lines)):
             item_index = self.menu_start_index + i
@@ -96,6 +94,7 @@ class SingleSelectVerticalScrollMenu():
                 # highlighted item
                 display.fill_rect(0, ((i * (line_height+spacer))-1) +
                                   pixel_y_shift, 128, line_height, 1)
+                # selected
                 display.text('*' + self.items[item_index] if self.selected == self.items[item_index] else self.items[item_index], 0,
                              (i * (line_height+spacer))+pixel_y_shift, 0)
             else:
@@ -104,42 +103,46 @@ class SingleSelectVerticalScrollMenu():
         display.show()
 
     def scroll(self, index):
+        # TODO implement rotary range wrap. When at the top (0), go to bottom if moving line up and vise versa.
         if index > self.menu_start_index + (self.total_lines-1):
             self.menu_start_index += 1
         if index < self.menu_start_index:
             self.menu_start_index -= 1
+        
 
 
 class NumericalValueRangeMenu():
     global display
     """A menu type that lets a user change a numerical value within a specified range, increment can also be changed"""
 
-    def __init__(self, name: str, selected_value: int, new_value: int, start: int, stop: int) -> None:
+    def __init__(self, name: str, *, selected_value: int, new_value: int = 0, start: int = 0, stop: int = 100, increment: int = 1) -> None:
         self.name = name
         self.selected_value = selected_value
         self.new_value = new_value
         self.start = start
         self.stop = stop
+        self.increment = increment
 
     def set_selected(self, selection) -> None:
         self.selected_value = selection
 
-    def get_selected(self) -> int:
-        return self.selected_value
-
     def scroll(self, new_value: int) -> None:
         self.new_value = new_value
-
-    def get_start_stop(self) -> tuple[int, int]:
-        return (self.start, self.stop)
 
     def display_menu(self) -> None:
         display.fill(0)
         display.text(self.name, 2, 4, 1)
         display.rect(0, 0, 128, 15, 1)
+        # old value
+        display.text(f'Old: {self.selected_value}', 0, 20, 1)
         if self.new_value == self.selected_value:
-            text = '*'+str(self.new_value)
+            text = f'New: *{self.new_value}'
         else:
-            text = str(self.new_value)
-        display.text(text, 46, 36, 1)
+            text = f'New: {self.new_value}'
+        display.fill_rect(0, 30, 128, 10, 1)
+        display.text(text, 0, 31, 0)
         display.show()
+
+def remove_vowels(word: str) -> str:
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    return ''.join([char for char in word if char not in vowels])
